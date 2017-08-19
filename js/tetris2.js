0d3ed4b77cb33c0804f777625595b8f311d062d5
sunq <1585437938@qq.com>
2017-08-19 22:53:10 +0800

    修复两个bug，并整理代码

--- js/tetris2.js
+++ js/tetris2.js
@@ -6,9 +6,9 @@ var tds = [],
     eggNum=0,
     timer,
     gameSpeed = 200,
-    
     headerTr=0,
-    headertd=0;
+    headertd=0,
+    preDirection;
 
 
 /*画棋盘 把所有td装到一个二维数组里*/
@@ -27,151 +27,70 @@ function Snake() {
 }
 Snake.prototype = {
     snakeBody:[],
-    turnLeft:function () {
+    turn:function () {
+        var direction = arguments[0];
+
+        // 限制回退操作
+        if((preDirection=="left"&&direction=="right")||(preDirection=="right"&&direction=="left")){
+            return;
+        }else if((preDirection=="up"&&direction=="down")||(preDirection=="down"&&direction=="up")){
+            return;
+        }
+        preDirection = direction;
+
         clearInterval(timer);
 
-        headertd-=1;
+        if(direction=="left"){
+            headertd-=1;
+        }else if(direction=="right"){
+            headertd+=1;
+        }else if(direction=="up"){
+            headerTr-=1;
+        }else if(direction=="down"){
+            headerTr+=1;
+        }
 
         if(headertd>29||headerTr>29||headertd<0||headerTr<0){
             clearInterval(timer);
             alert("已撞晕，game over");
             window.location.reload();
-        }else {
-            this.snakeBody[0].style.backgroundColor = "white";
-
-            if(snake.snakeBody[snake.snakeBody.length-1]==egg){
-                makeEgg();
-            }else {
-                this.snakeBody.shift();
-            }
-
-            this.snakeBody.push(tds[headerTr][headertd]);
-
-            for(var b=0;b<this.snakeBody.length;b++){
-                this.snakeBody[b].style.backgroundColor = "black";
-                if(tds[headerTr][headertd]==this.snakeBody[b]&&tds[headerTr][headertd]!=this.snakeBody[this.snakeBody.length-1]){
-                    alert("jj");
-                }
-            }
-
-
-        }
-
-        timer = setInterval(function () {
-            snake.turnLeft();
-        },gameSpeed);
-    },
-    turnRight:function () {
-        clearInterval(timer);
-
-        headertd+=1;
-
-        if(headertd>29||headerTr>29||headertd<0||headerTr<0){
+        }else if(biteMyself()){
             clearInterval(timer);
-            alert("已撞晕，game over");
+            alert("咬自己了，game over");
             window.location.reload();
         }else {
-            this.snakeBody[0].style.backgroundColor = "white";
-
             if(snake.snakeBody[snake.snakeBody.length-1]==egg){
                 makeEgg();
+                snake.snakeBody[0].style.backgroundColor = "black";
             }else {
-                this.snakeBody.shift();
-            }
-
-            this.snakeBody.push(tds[headerTr][headertd]);
-            for(var b=0;b<this.snakeBody.length;b++){
-                this.snakeBody[b].style.backgroundColor = "black";
-                if(tds[headerTr][headertd]==this.snakeBody[b]&&tds[headerTr][headertd]!=this.snakeBody[this.snakeBody.length-1]){
-                    alert("jj");
-                }
-            }
-        }
-
-        timer = setInterval(function () {
-            snake.turnRight();
-        },gameSpeed);
-    },
-    turnDown:function () {
-        clearInterval(timer);
-
-        headerTr+=1;
-
-        if(headertd>29||headerTr>29||headertd<0||headerTr<0){
-            alert("已撞晕，game over");
-            window.location.reload();
-        }else {
-            this.snakeBody[0].style.backgroundColor = "white";
-
-            if(snake.snakeBody[snake.snakeBody.length-1]==egg){
-                makeEgg();
-            }else {
+                this.snakeBody[0].style.backgroundColor = "white";
                 this.snakeBody.shift();
             }
 
             this.snakeBody.push(tds[headerTr][headertd]);
 
-            for(var b=0;b<this.snakeBody.length;b++){
-                this.snakeBody[b].style.backgroundColor = "black";
-                if(tds[headerTr][headertd]==this.snakeBody[b]&&tds[headerTr][headertd]!=this.snakeBody[this.snakeBody.length-1]){
-                    alert("jj");
-                }
-            }
+
+            this.snakeBody[this.snakeBody.length-1].style.backgroundColor = "black";
         }
-
-
         timer = setInterval(function () {
-            snake.turnDown();
-        },gameSpeed);
-    },
-    turnTop:function () {
-        clearInterval(timer);
-
-        headerTr-=1;
-        if(headertd>29||headerTr>29||headertd<0||headerTr<0){
-            clearInterval(timer);
-            alert("已撞晕，game over");
-            window.location.reload();
-        }else {
-            this.snakeBody[0].style.backgroundColor = "white";
-
-            if(snake.snakeBody[snake.snakeBody.length-1]==egg){
-                makeEgg();
-            }else {
-                this.snakeBody.shift();
-            }
-
-            this.snakeBody.push(tds[headerTr][headertd]);
-
-            for(var b=0;b<this.snakeBody.length;b++){
-                this.snakeBody[b].style.backgroundColor = "black";
-                if(tds[headerTr][headertd]==this.snakeBody[b]&&tds[headerTr][headertd]!=this.snakeBody[this.snakeBody.length-1]){
-                    alert("jj");
-                }
-            }
-        }
-
-        timer = setInterval(function () {
-            snake.turnTop();
+            snake.turn(direction);
         },gameSpeed);
     }
 }
 var snake = new Snake();
-snake.snakeBody.push(tds[headerTr][headertd]);
-for(var a=0;a<snake.snakeBody.length;a++){
-    snake.snakeBody[a].style.backgroundColor = "black";
-}
+snake.snakeBody.push(tds[0][0]);
+snake.snakeBody[0].style.backgroundColor = "black";
 makeEgg();
 
 window.addEventListener("keydown",function (e) {
     if(e.keyCode=="40"){
-        snake.turnDown();
+        snake.turn("down");
     }else if(e.keyCode=="37"){
-        snake.turnLeft();
+        snake.turn("left");
     }else if(e.keyCode=="39"){
-        snake.turnRight();
+        snake.turn("right");
     }else if(e.keyCode=="38"){
-        snake.turnTop();
+        snake.turn("up");
     }
 });
 
@@ -194,4 +113,13 @@ function makeEgg() {
     }else if(eggNum==8) {
         gameSpeed -= 20;
     }
+}
+
+function biteMyself() {
+    for(var b=0,snakelength=snake.snakeBody.length-1;b<snakelength;b++){
+        if(snake.snakeBody[snakelength]==snake.snakeBody[b]){
+            return true;
+        }
+    }
+    return false;
 }
